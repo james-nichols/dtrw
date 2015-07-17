562cdc8d56d696f49f4875947978db7b415b6dfd
James Nichols <james.ashton.nichols@gmail.com>
2015-07-17 11:01:58 +1000

    updated Example 2, now with more data points

--- dtrw_reactions_paper/Example_2.py
+++ dtrw_reactions_paper/Example_2.py
@@ -66,7 +66,7 @@ class DTRW_subdiffusive_two_way(DTRW_subdiffusive):
     def calc_nu(self):
         """Likelihood of birth happening at i"""
         if self.nus == None:
-            self.nus = [np.zeros((X.shape[0], X.shape[1], self.N)) for X in self.Xs]
+            self.nus = [np.zeros((X.shape[0], X.shape[1])) for X in self.Xs]
     
         self.nus[0][:,:] = (1. - np.exp(-self.k_2)) * self.Xs[1][:,:,self.n]
         #self.nus[0][:,:] = -self.k_2 * self.Xs[1][:,:,self.n]
@@ -78,44 +78,25 @@ alpha_1 = 0.5
 D_alpha = 1.
 T = 0.1
 k_1 = 1.
-dTs = [1.e-3, 5.e-4, 1.e-4, 5.e-5, 1.e-5]
 
-dXs = [0.1, 0.05, 0.02, 0.01]
-dTs = [.001, .0001, .00001, .000001]
-dXs = [0.1, 0.05, 0.02]
+r = 1.
 
-dTs = [1e-5, 1e-5, 1e-6, 1e-6]
-nXs = [10, 20, 40, 80]#[21, 41, 61, 81]
+dXs = [0.2, 0.1, 0.05, 0.025, 0.0125]
 
-#for dT in dTs:
-for j in range(len(nXs)):
-    #dX = dXs[j]
-    nX  = nXs[j]
-    dT = dTs[j]
+for dX in dXs:
+     
+    dT = pow(dX * dX / (2.0 * D_alpha), 1.0 / alpha_1)
+
+    xs = np.arange(-1.0, 1.0+dX, dX)
     
-    #dX = math.sqrt(2. * D_alpha * pow(dT, alpha_1))
-
-    dX = 2. / nX
-    L = math.floor(1.0 / dX)
-    x_r = np.arange(0., 1.+dX, dX)
-    x_l = np.arange(-dX, -1.-dX, -dX)
-    xs = np.append(x_l[::-1], x_r)
-    
-    #xs = np.linspace(-1., 1., nX, endpoint=True)
-    n_points = len(xs)
-    
-    r = 2. * D_alpha * pow(dT, alpha_1) / (dX * dX)
-    while r > 1.0:
-        dT = 0.5 * dT
-
     # We make the init conditions slightly bigger for the zero flux boundaries
-    a_init = np.zeros(n_points)
-    a_init[n_points / 2 ] = float(n_points)/2.
-    b_init = np.zeros(n_points)
-    
-    N = int(math.floor(T / dT))+1
-    N_1 = int(math.floor(T / dT))+1
-    history_length = N
+    a_init = np.zeros(xs.shape)
+    a_init[xs.shape[0] / 2 ] = 1.0 / dX #float(n_points)/2.
+    b_init = np.zeros(xs.shape)
+
+    N = int(round(T / dT) + 1)
+    N_1 = int(round(T / dT) + 1)
+    history_length = N + 1
 
     ts = np.array(np.arange(N) * dT)
     ts_1 = np.array(np.arange(N_1) * dT)
@@ -123,27 +104,28 @@ for j in range(len(nXs)):
     # Calculate r for diffusive case so as to get the *same* dT as the subdiffusive case
     omega = dT / (dX * dX / (2.0 * D_alpha))
 
-    print "DTRW for dX =", dX, "dT =", dT, "omega =", omega, "N =", N
-    print "DTRW subdiff for dX =", dX, "dT =", dT, "r =", r, "alpha =", alpha_1, "N =", N_1
+    #print "DTRW for dX =", dX, "dT =", dT, "omega =", omega, "N =", N
+    print "Solving DTRW subdiff for dX =", dX, "dT =", dT, "r =", r, "alpha =", alpha_1, "N =", N_1
 
     bc = BC_zero_flux()
 
     dtrw_sub_1 = DTRW_subdiffusive_two_way([a_init, b_init], N_1, alpha_1, k_1 * dT, k_1 * dT, r=r, history_length=N_1, boundary_condition=bc)
-    dtrw = DTRW_diffusive_two_way([a_init, b_init], N, omega, k_1 * dT, k_1 * dT, history_length=2, boundary_condition=bc)
+    #dtrw = DTRW_diffusive_two_way([a_init, b_init], N, omega, k_1 * dT, k_1 * dT, history_length=2, boundary_condition=bc)
 
-#    dtrw.solve_all_steps()
-    print "Exp case solved"
+    #dtrw.solve_all_steps()
+    #print "Exp case solved"
     dtrw_sub_1.solve_all_steps()
-    print "alpha =", alpha_1, "case solved"
     
     dtrw_file_name = "DTRW_dT_{0:f}_dX_{1:f}.csv".format(dT, dX)
 
-    np.savetxt("a_" + dtrw_file_name, dtrw.Xs[0][0,:,-1], delimiter=",")
-    np.savetxt("b_" + dtrw_file_name, dtrw.Xs[1][0,:,-1], delimiter=",")
+    #np.savetxt("a_" + dtrw_file_name, dtrw.Xs[0][0,:,-1], delimiter=",")
+    #np.savetxt("b_" + dtrw_file_name, dtrw.Xs[1][0,:,-1], delimiter=",")
 
     dtrw_file_name_alpha_1 = "DTRW_dT_{0:f}_dX_{1:f}_alpha_{2:f}.csv".format(dT, dX, alpha_1)
     np.savetxt("a_" + dtrw_file_name_alpha_1, dtrw_sub_1.Xs[0][0,:,-1], delimiter=",")
     np.savetxt("b_" + dtrw_file_name_alpha_1, dtrw_sub_1.Xs[1][0,:,-1], delimiter=",")
+    np.savetxt("a_2ndLast_" + dtrw_file_name_alpha_1, dtrw_sub_1.Xs[0][0,:,-2], delimiter=",")
+    np.savetxt("b_2ndLast_" + dtrw_file_name_alpha_1, dtrw_sub_1.Xs[1][0,:,-2], delimiter=",")
 
     t_alpha_1 = "T_dT_{0:f}_alpha_{1:f}.csv".format(dT, alpha_1)
     np.savetxt(t_alpha_1, ts_1, delimiter=",")
