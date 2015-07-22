f82d30b984c6887a9ce447e48c79eef2212699da
James Nichols <james.ashton.nichols@gmail.com>
2015-07-22 10:53:28 +1000

    added centroid results choice

--- dtrw_reactions_paper/Example_2.py
+++ dtrw_reactions_paper/Example_2.py
@@ -73,7 +73,6 @@ class DTRW_subdiffusive_two_way(DTRW_subdiffusive):
         self.nus[1][:,:] = (1. - np.exp(-self.k_1)) * self.Xs[0][:,:,self.n]
         #self.nus[1][:,:] = -self.k_1 * self.Xs[0][:,:,self.n]
 
-
 alpha_1 = 0.5
 D_alpha = 1.
 T = 0.1
@@ -81,35 +80,46 @@ k_1 = 1.
 
 r = 1.
 
-dXs = [1./2, 1./5., 1./7., 1./10., 1./15., 1./20., 1./30.]
+# Do we want results on the grid points, or in the centroids?
+use_grid_aligned = False
+
+if use_grid_aligned:
+    dXs = [1./2, 1./5., 1./7., 1./10., 1./15., 1./20., 1./30.]          # Choose this for grid aligned results
+else:
+    dXs = [2./3., 2./5., 2./7., 2./11., 2./15., 2./21., 2./31., 2./41.] # Choose this for centroid results
+
+soln = np.loadtxt("Soln_at_t01.dat")
 
 for dX in dXs:
      
     dT = pow(dX * dX / (2.0 * D_alpha), 1.0 / alpha_1)
 
-    xs = np.arange(-1.0, 1.0+dX, dX)
-    
+    if use_grid_aligned:
+        xs = np.arange(-1.0, 1.0+dX, dX)                        # Choose this for grid aligned results
+    else:
+        xs = np.arange(-1.0+0.5*dX, 1.0, dX)                    # Choose this for centroid results
+
     # We make the init conditions slightly bigger for the zero flux boundaries
     a_init = np.zeros(xs.shape)
-    a_init[xs.shape[0] / 2 ] = float(xs.shape[0]-1)/2.
+    if use_grid_aligned:
+        a_init[xs.shape[0] / 2 ] = float(xs.shape[0]-1)/2.     # Choose this for grid aligned results
+    else:
+        a_init[xs.shape[0] / 2 ] = 1./dX                       # Choose this for centroid results
     b_init = np.zeros(xs.shape)
 
     N = int(round(T / dT) + 1)
-    N_1 = int(round(T / dT) + 1)
     history_length = N + 1
 
     ts = np.array(np.arange(N) * dT)
-    ts_1 = np.array(np.arange(N_1) * dT)
 
     # Calculate r for diffusive case so as to get the *same* dT as the subdiffusive case
     omega = dT / (dX * dX / (2.0 * D_alpha))
 
-    #print "DTRW for dX =", dX, "dT =", dT, "omega =", omega, "N =", N
-    print "Solving DTRW subdiff for dX =", dX, "dT =", dT, "r =", r, "alpha =", alpha_1, "N =", N_1
+    print "Solving DTRW subdiff for dX =", dX, "dT =", dT, "r =", r, "alpha =", alpha_1, "N =", N
 
     bc = BC_zero_flux()
 
-    dtrw_sub_1 = DTRW_subdiffusive_two_way([a_init, b_init], N_1, alpha_1, k_1 * dT, k_1 * dT, r=r, history_length=N_1, boundary_condition=bc)
+    dtrw_sub_1 = DTRW_subdiffusive_two_way([a_init, b_init], N, alpha_1, k_1 * dT, k_1 * dT, r=r, history_length=N, boundary_condition=bc)
 
     dtrw_sub_1.solve_all_steps()
     
@@ -122,5 +132,5 @@ for dX in dXs:
     np.savetxt("b_2ndLast_" + dtrw_file_name_alpha_1, dtrw_sub_1.Xs[1][0,:,-2], delimiter=",")
 
     t_alpha_1 = "T_dT_{0:f}_alpha_{1:f}.csv".format(dT, alpha_1)
-    np.savetxt(t_alpha_1, ts_1, delimiter=",")
+    np.savetxt(t_alpha_1, ts, delimiter=",")
 
