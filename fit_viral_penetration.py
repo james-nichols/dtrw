6441615e900c72e1d2a24af16fa2ab8610a093a1
James Nichols <james.ashton.nichols@gmail.com>
2015-11-12 15:39:59 +1100

    getting closer to good fit

--- fit_viral_penetration.py
+++ fit_viral_penetration.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 
 import numpy as np
-import scipy.interpolate
-import time, csv
+import scipy.interpolate, scipy.special
+import time, csv, math
 from dtrw import *
 
 from mpl_toolkits.mplot3d import Axes3D
@@ -30,27 +30,59 @@ def produce_soln(params, T, L, dX, x_fit, y_fit):
     
     D_alpha, alpha, LHS = params
 
-    #alpha = min(1.0, alpha)
-    #alpha = max(0.0, alpha)
-    #D_alpha = max(0.0, D_alpha)
-    #LHS = max(0.0, LHS)
+    dT_pre = pow((dX * dX / (2.0 * D_alpha)), 1./alpha)
+    N = int(math.ceil(T / dT_pre))
     
-    print D_alpha, alpha, LHS
+    # Now find r to get an *exact* D_alpha
+    dT = T / N
 
-    dT = pow((dX * dX / (2.0 * D_alpha)), 1./alpha)
-    N = int(math.floor(T / dT))
+    # Calculate r for diffusive case so as to get the *same* dT as the subdiffusive case
+    r = dT / (dX * dX / (2.0 * D_alpha))
+    X_init = np.zeros(np.floor(L / dX))
+    
+    print "Params: ", D_alpha, alpha, LHS, " N =", N,
+    
+    dtrw_sub = DTRW_subdiffusive(X_init, N, alpha, r = r, history_length=N, boundary_condition=BC_Dirichelet([LHS, 0.0]))
+    dtrw_sub.solve_all_steps()
+    
+    interp = scipy.interpolate.interp1d(np.arange(0., L, dX), dtrw_sub.Xs[0][:,:,-1])
+    print "Square err:", ((interp(x_fit).flatten() - y_fit) * (interp(x_fit).flatten() - y_fit)).sum()
+    
+    fit = (interp(x_fit).flatten())
+    goal = (y_fit)
+
+    #return ((interp(x_fit).flatten() - y_fit) * (interp(x_fit).flatten() - y_fit)).sum()
+    return ((fit - goal) * (fit - goal)).sum()
+
+def produce_diff_soln(params, T, L, dX, x_fit, y_fit):
+    
+    D_alpha, LHS = params
+
+    dT_pre = dX * dX / (2.0 * D_alpha)
+    N = int(math.ceil(T / dT_pre))
+
+    dT = T / N
 
     # Calculate r for diffusive case so as to get the *same* dT as the subdiffusive case
     r = dT / (dX * dX / (2.0 * D_alpha))
 
     X_init = np.zeros(np.floor(L / dX))
-
-    dtrw_sub = DTRW_subdiffusive(X_init, N, alpha, history_length=N, boundary_condition=BC_Dirichelet([LHS, 0.0]))
-    dtrw_sub.solve_all_steps()
     
-    interp = scipy.interpolate.interp1d(np.arange(0., L, dX), dtrw_sub.Xs[0][:,:,-1])
+    print "Params: ", D_alpha, LHS, " N =", N,
     
+    dtrw = DTRW_diffusive(X_init, N, r, history_length=N, boundary_condition=BC_Dirichelet([LHS, 0.0]))
+    dtrw.solve_all_steps()
+    
+    interp = scipy.interpolate.interp1d(np.arange(0., L, dX), dtrw.Xs[0][:,:,-1])
+    print "Square err:", ((interp(x_fit).flatten() - y_fit) * (interp(x_fit).flatten() - y_fit)).sum()
     return ((interp(x_fit).flatten() - y_fit) * (interp(x_fit).flatten() - y_fit)).sum()
+
+    fit = np.log(interp(x_fit).flatten())
+    goal = np.log(y_fit)
+
+    #return ((interp(x_fit).flatten() - y_fit) * (interp(x_fit).flatten() - y_fit)).sum()
+    return ((fit - goal) * (fit - goal)).sum()
+
 
 
 labels = []
@@ -79,19 +111,24 @@ num_depth_bins = 20
 
 #depth_hist, depth_bins = np.histogram(nz_depth, bins=np.linspace(nz_depth.min(), nz_depth.max(), num_depth_bins, endpoint=True))
 depth_hist, depth_bins = np.histogram(nz_depth, num_depth_bins, density=True)
+bin_cent = (depth_bins[1:]+depth_bins[:-1])/2.0
 
-T = 1.0
+T = 10.0
 L = nz_depth.max()
-dX = L / 400.0
+dX = L / 50.0
 
-D_alpha = 2.0
-alpha = 0.8
+D_alpha = 1.0
+alpha = 0.7
 init_params = [D_alpha, alpha, depth_hist[0]]
 
 #fit = scipy.optimize.leastsq(produce_soln, init_params, args=(T, L, dX, (depth_bins[1:]+depth_bins[:-1])/2.0, depth_hist), options={'disp': True})
-fit = scipy.optimize.fmin_slsqp(produce_soln, init_params, args=(T, L, dX, (depth_bins[1:]+depth_bins[:-1])/2.0, depth_hist), bounds=[(0.0, np.Inf),(0.0, 1.0), (0.0, np.Inf)])
+fit = scipy.optimize.fmin_slsqp(produce_soln, init_params, args=(T, L, dX, bin_cent, depth_hist), \
+                                bounds=[(0.0, 100.0),(0.55, 1.0), (0.0, 10.0)], epsilon = 1.0e-5, acc=1.0e-9)
 
-pdb.set_trace()
+diff_init_params = [2.0*D_alpha, depth_hist[0]]
+diff_fit = scipy.optimize.fmin_slsqp(produce_diff_soln, diff_init_params, args=(T, L, dX, bin_cent, depth_hist), \
+                                bounds=[(0.0, 100.0), (0.0, 10.0)], epsilon = 1.0e-5, acc=1.0e-9)
+
 
 D_alpha, alpha, LHS = fit
 
@@ -105,91 +142,38 @@ xs = np.arange(0.0, L, dX)
 dtrw_sub = DTRW_subdiffusive(X_init, N, alpha, history_length=N, boundary_condition=BC_Dirichelet([LHS, 0.0]))
 dtrw_sub.solve_all_steps()
 
-alpha = 0.75
-D_alpha = 4.0
-dT = pow((dX * dX / (2.0 * D_alpha)), 1./alpha)
-N = int(math.floor(T / dT))
-dtrw_test = DTRW_subdiffusive(X_init, N, alpha, history_length=N, boundary_condition=BC_Dirichelet([LHS, 0.0]))
-dtrw_test.solve_all_steps()
-pdb.set_trace()
-plt.bar((depth_bins[1:]+depth_bins[:-1])/2.0, depth_hist)
-plt.plot(xs, dtrw_sub.Xs[0][:,:,-1].T, xs, dtrw_test.Xs[0][:,:,-1].T)
-plt.plot(xs, dtrw_test.Xs[0][:,:,-1].T)
-plt.show()
+slope, offset = np.linalg.lstsq(np.vstack([bin_cent, np.ones(len(bin_cent))]).T, np.log(depth_hist).T)[0]
+exp_fit = np.exp(offset + xs * slope)
+ 
+D_alpha, LHS = diff_fit
 
-n_points = 100
-T = 2.0
-
-alpha = 0.75
-D_alpha = 0.1
-
-dT = pow((dX * dX / (2.0 * D_alpha)), 1./alpha)
-N = int(math.floor(T / dT))
-
-L = 5.0
-dX = L / n_points
-
-X_init = np.zeros(n_points)
-X_init[n_points / 2] = 1.0
-
-T = 2.0
-
-alpha = 0.75
-D_alpha = 0.1
-
-dT = pow((dX * dX / (2.0 * D_alpha)), 1./alpha)
+dT = dX * dX / (2.0 * D_alpha)
 N = int(math.floor(T / dT))
 
 # Calculate r for diffusive case so as to get the *same* dT as the subdiffusive case
 r = dT / (dX * dX / (2.0 * D_alpha))
-
-print "Diffusive sim with dT =", dT, "N =", N, "alpha =", alpha, "diffusion matching r =", r
-
-dtrw = DTRW_diffusive(X_init, N, r)
-dtrw_sub = DTRW_subdiffusive(X_init, N, alpha, history_length=N)
-
-print "Left jump probs: ", dtrw.lam[:,:,0]
-print "Right jump probs: ", dtrw.lam[:,:,1]
-
+X_init = np.zeros(np.floor(L / dX))
+    
+dtrw = DTRW_diffusive(X_init, N, r, history_length=N, boundary_condition=BC_Dirichelet([LHS, 0.0]))
 dtrw.solve_all_steps()
-dtrw_sub.solve_all_steps()
 
-print "Solutions computed, now creating animation..."
+diff_analytic_soln = LHS * (1.0 - scipy.special.erf(xs / math.sqrt(4. * D_alpha * T)))
+    
+fig = plt.figure(figsize=(16,8))
+ax1 = fig.add_subplot(1, 2, 1)
+bar1 = ax1.bar(bin_cent, depth_hist)
+line1, = ax1.plot(xs, dtrw_sub.Xs[0][:,:,-1].T, 'r.-')
+line2, = ax1.plot(xs, dtrw.Xs[0][:,:,-1].T, 'g.-')
+line4, = ax1.plot(xs, diff_analytic_soln, 'gx')
+line3, = ax1.plot(xs, exp_fit, 'b')
 
-xs = np.linspace(0., L, n_points, endpoint=False)
+ax2 = fig.add_subplot(1, 2, 2)
+ax2.semilogy(bin_cent, depth_hist, 'o')
+ax2.semilogy(xs, dtrw_sub.Xs[0][:,:,-1].T, 'r.-')
+ax2.semilogy(xs, dtrw.Xs[0][:,:,-1].T, 'g.-')
+ax2.semilogy(xs, diff_analytic_soln, 'gx')
+ax2.semilogy(xs, exp_fit, 'b')
 
-fig = plt.figure(figsize=(8,8))
-plt.xlim(L/4,3*L/4)
-plt.ylim(0,0.1)
-plt.xlabel('x')
-line1, = plt.plot([],[],'r-')
-line2, = plt.plot([],[],'g-')
-line3, = plt.plot([],[],'bx')
-line4, = plt.plot([],[],'kx')
-plt.legend([line1, line2, line3, line4], ["Analytic diffusion", "Analytic subdiffusion, alpha=3/4", "DTRW diffusion", "DTRW Sub-diffusion, alpha=3/4"])
-
-def update(i, line1, line2, line3, line4):
-    line1.set_data(xs,dtrw.Xs[0][:,:,i])
-    line2.set_data(xs,dtrw_sub.Xs[0][:,:,i])
-    if i == 0:
-        analytic_soln = X_init
-    else:
-        analytic_soln = (1./math.sqrt(4. * math.pi * float(i) * dT * D_alpha)) * np.exp( - (xs - 2.5) * (xs - 2.5) / (4. * D_alpha * float(i) * dT)) * dX
-    line3.set_data(xs, analytic_soln)
-    line4.set_data(xs,dtrw_sub.Xs[0][:,:,i])
-    return line1, line2, line3, line4
-
-# call the animator. blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, update, 
-        frames=N, fargs=(line1, line2, line3, line4), interval=10)
-
-import inspect, os, subprocess
-exec_name =  os.path.splitext(os.path.basename(inspect.getfile(inspect.currentframe())))[0]
-git_tag = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).replace('\n', '')
-
-file_name = '{0}_{1}.mp4'.format(exec_name, git_tag)
-print "Saving animation to", file_name
-
-anim.save(file_name, fps=24)
+plt.legend([bar1, line1, line2, line3], ["Viral Penetration Hist", "Subdiffusion fit", "Diffusion fit", "Exponential fit"])
 plt.show()
 
