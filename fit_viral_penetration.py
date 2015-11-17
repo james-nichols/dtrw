43934ae415398920fa42ec9eaf9dcac37003bb93
James Nichols <james.ashton.nichols@gmail.com>
2015-11-17 14:23:32 +1100

    fit survival function with survival functions...

--- fit_viral_penetration.py
+++ fit_viral_penetration.py
@@ -48,6 +48,12 @@ def produce_subdiff_soln(params, T, L, dX):
     
     return dtrw_sub.Xs[0][:,:,-1]
 
+def produce_subdiff_soln_survival(params, T, L, dX):
+    
+    D_alpha, alpha, LHS = params
+
+    soln = produce_subdiff_soln(params, T, L, dX)
+    return 1.0 - np.array([np.trapz(soln.flatten()[:i], dx=dX) for i in range(1,soln.size+1)]) / np.trapz(soln.flatten(), dx=dX)
 
 def produce_diff_soln(params, T, L, dX, xs):
     
@@ -56,23 +62,34 @@ def produce_diff_soln(params, T, L, dX, xs):
 
     return LHS * (1.0 - scipy.special.erf(xs / math.sqrt(4. * D_alpha * T)))
 
+def produce_diff_soln_survival(params, T, L, dX, xs):
+    
+    D_alpha, LHS = params
+    D_alpha = max(D_alpha, 0.0)
+    
+    integral = LHS * (xs - xs * scipy.special.erf(xs / math.sqrt(4. * D_alpha * T)) - math.sqrt(4. * D_alpha * T / math.pi) * (np.exp(-xs * xs / (4. * D_alpha * T)) - 1.))
+    full_integral = LHS * math.sqrt(4. * D_alpha * T / math.pi)
+    
+    return 1.0 - integral / full_integral 
+
 def lsq_subdiff(params, T, L, dX, x_fit, y_fit):
 
     print "Subdiff params: ", params[0], params[1], params[2],
-    soln = produce_subdiff_soln(params, T, L, dX)
+    #soln = produce_subdiff_soln(params, T, L, dX)
+    # If we're fitting to a survival function, we must fit the partial integral of the solution...
+    soln = produce_subdiff_soln_survival(params, T, L, dX)
+
     interp = scipy.interpolate.interp1d(np.arange(0., L+dX, dX), soln)
     
-    fit = np.log(interp(x_fit).flatten())
-    goal = np.log(y_fit)
+    fit = (interp(x_fit).flatten())
+    goal = (y_fit)
     sq_err = ((fit - goal) * (fit - goal)).sum()
     print "Square err:", sq_err
     return sq_err
 
-
 def lsq_diff(params, T, L, dX, x_fit, y_fit):
-
-    fit = np.log(produce_diff_soln(params, T, L, dX, x_fit))
-    goal = np.log(y_fit)
+    fit = (produce_diff_soln_survival(params, T, L, dX, x_fit))
+    goal = (y_fit)
     sq_err = ((fit - goal) * (fit - goal)).sum()
 
     return sq_err
@@ -119,44 +136,47 @@ dX = L / 100.0
 D_alpha = 1.0
 alpha = 0.7
 init_params = [D_alpha, alpha, 1.0]
-# Last minimisation got close to: 17.8313421414 0.657650087016 0.740546304837
-#init_params = [17.8313421414, 0.657650087016, 0.740546304837]
-#subdiff_fit = init_params
-
-#fit = scipy.optimize.leastsq(produce_soln, init_params, args=(T, L, dX, (depth_bins[1:]+depth_bins[:-1])/2.0, depth_hist), options={'disp': True})
-subdiff_fit = scipy.optimize.fmin_slsqp(lsq_subdiff, init_params, args=(T, L, dX, surv_func_x, surv_func_y), \
-                                bounds=[(0.0, 30.0),(0.55, 1.0), (0.0, 10.0)], epsilon = 1.0e-8, acc=1.0e-6)
+# Last minimisation got close to:
+#diff_fit = [ 5.28210775, 0.95847065]
+#subdiff_fit = [ 15.07811124, 0.55, 0.99997347]
 
 diff_init_params = [2.0*D_alpha, 1.0]
 diff_fit = scipy.optimize.fmin_slsqp(lsq_diff, diff_init_params, args=(T, L, dX, surv_func_x, surv_func_y), \
                                 bounds=[(0.0, np.Inf), (0.0, np.Inf)], epsilon = 1.0e-8, acc=1.0e-9)
 
+print 'Diffusion fit parameters:', diff_fit
+
+fit = scipy.optimize.leastsq(produce_soln, init_params, args=(T, L, dX, (depth_bins[1:]+depth_bins[:-1])/2.0, depth_hist), options={'disp': True})
+subdiff_fit = scipy.optimize.fmin_slsqp(lsq_subdiff, init_params, args=(T, L, dX, surv_func_x, surv_func_y), \
+                                bounds=[(0.0, 30.0),(0.55, 1.0), (0.0, 10.0)], epsilon = 1.0e-8, acc=1.0e-6)
+
 xs = np.arange(0.0, L+dX, dX)
 dtrw_sub_soln = produce_subdiff_soln(subdiff_fit, T, L, dX)
 diff_analytic_soln = produce_diff_soln(diff_fit, T, L, dX, xs) 
+dtrw_sub_soln_survival = produce_subdiff_soln_survival(subdiff_fit, T, L, dX)
+diff_analytic_soln_survival = produce_diff_soln_survival(diff_fit, T, L, dX, xs) 
 
 print 'Subdiffusion fit parameters:', subdiff_fit
-print 'Diffusion fit parameters:', diff_fit
 
 slope, offset = np.linalg.lstsq(np.vstack([surv_func_x, np.ones(len(surv_func_x))]).T, np.log(surv_func_y).T)[0]
 exp_fit = np.exp(offset + xs * slope)
     
 fig = plt.figure(figsize=(16,8))
 ax1 = fig.add_subplot(1, 2, 1)
-#bar1 = ax1.bar(bin_cent, depth_hist)
 bar1, = ax1.plot(surv_func_x, surv_func_y, 'b.-')
-line1, = ax1.plot(xs, dtrw_sub_soln.T, 'r.-')
-line2, = ax1.plot(xs, diff_analytic_soln, 'g.-')
+line1, = ax1.plot(xs, dtrw_sub_soln_survival.T, 'r.-')
+line2, = ax1.plot(xs, diff_analytic_soln_survival, 'g.-')
 line3, = ax1.plot(xs, exp_fit, 'b')
+ax1.set_title('Survival function vs fits')
 
 ax2 = fig.add_subplot(1, 2, 2)
-#ax2.semilogy(bin_cent, depth_hist, 'o')
 ax2.semilogy(surv_func_x, surv_func_y, 'b.-')
-ax2.semilogy(xs, dtrw_sub_soln.T, 'r.-')
-ax2.semilogy(xs, diff_analytic_soln, 'g.-')
+ax2.semilogy(xs, dtrw_sub_soln_survival.T, 'r.-')
+ax2.semilogy(xs, diff_analytic_soln_survival, 'g.-')
 ax2.semilogy(xs, exp_fit, 'b')
+ax2.set_title('Logarithm of survival function vs fits')
 
-plt.legend([bar1, line1, line2, line3], ["Viral Penetration Hist", "Subdiffusion fit", "Diffusion fit", "Exponential fit"])
+plt.legend([bar1, line1, line2, line3], ["Viral survival func", "Subdiffusion fit, alpha={0}, D_alpha={1}".format(subdiff_fit[1],subdiff_fit[0]), "Diffusion fit, D_alpha={0}".subdiff_fit[0], "Exponential fit"])
 pp.savefig()
 #pp.attach_note("Subdiffusion Parameters: " + subdiff_fit)
 #pp.attach_note("Diffusion Parameters: " + diff_fit)
